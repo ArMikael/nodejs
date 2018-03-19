f507ffb36b02a40722582b13e41b4cbd9166c60b
Michael Treser <armikael2@gmail.com>
2018-03-19 08:56:18 +0200

    New ways to work with modules.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var user = require('./user');
+var User = require('./user');
 
-var michael = new user.User('Michael');
-var nataly = new user.User('Nataly');
+function run() {
+    var michael = new User('Michael');
+    var nataly = new User('Nataly');
 
-michael.hello(nataly);
+    michael.hello(nataly);
+}
+
+
+if (module.parent) { // Если этот модуль был вызван другим модулем
+    exports.run = run;
+} else { // Если файл был запущен сам по себе
+    run();
+}
