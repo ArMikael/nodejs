7b1d4ebe567cc0eb400eac34d34a969b934ca971
Michael Treser <armikael2@gmail.com>
2018-05-24 10:27:01 +0300

    Finished work on basic dojo-server with html/css/js request.

--- dojo-server.js
+++ dojo-server.js
@@ -1,21 +1,54 @@
 const http = require('http');
 
-http.createServer((req, res), function () {
+const html = `
+    <!doctype>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Dojo NodeJS Server</title>
+            <link rel="stylesheet" href="/app.css">
+            <script src="/app.js"></script>
+        </head>
+        <body>
+            <h1>Dojo NodeJS Server</h1>
+        </body>
+    </html>
+`;
+
+const css = `
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: Ivory;
+    }
+    
+    h1  {
+        padding: 20px;
+        background-color: Maroon;
+        color: white;
+    }
+`;
+
+const js = `
+    console.log('Dojo Server is up and running!');
+`;
+
+http.createServer(function (req, res) {
     switch (req.url) {
         case '/':
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end();
+            res.end(html);
 
         case '/app.css':
             res.writeHead(200, {'Content-Type': 'text/css'});
-            res.end();
+            res.end(css);
 
         case '/app.js':
             res.writeHead(200, {'Content-Type': 'text/javascript'});
-            res.end();
+            res.end(js);
 
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('Page not found.');
     }
-});
+}).listen(3000, ()=> console.log('Dojo is up and running!'));
