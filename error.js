5d989d21bb7052544201ba35bbd0162872e2146b
Michael Treser <armikael2@gmail.com>
2018-06-02 21:44:49 +0300

    keep working on error handler.

--- error.js
+++ error.js
@@ -1,9 +1,11 @@
+var util = require('util');
+
 function PhraseError (message) {
     this.message = message;
     Error.captureStackTrace(this, PhraseError); // второй параметр указывает до какого места отслеживать путь ошибки
 }
 
-utils.inherits(PhraseError, Error);
+util.inherits(PhraseError, Error);
 PhraseError.prototype.name = 'PhraseError';
 
 
@@ -13,13 +15,32 @@ function HttpError(status, message) {
     Error.captureStackTrace(this, HttpError);
 }
 
-utils.inherits(HttpError, Error);
+util.inherits(HttpError, Error);
 HttpError.prototype.name = 'HttpError';
 
 
 function getPhrase(name) {
     if (!phrases[name]) {
-        throw new PhraseError('Нет такой фразы ', + name);
+        throw new PhraseError("Phrase doesn't exists", + name);
     }
     return phrases[name];
+}
+
+function makePage(url) {
+    if (url != 'index.html') {
+        throw new HttpError(404, 'Page not found')
+    }
+    return util.format("%s, %s!", getPhrase('*****'), getPhrase('world'));
+}
+
+try {
+    var page = makePage('index.html');
+    console.log(page);
+
+} catch (e) {
+    if (e instanceof HttpError) {
+        console.log(e.status, e.message);
+    } else {
+        console.error('Error %s\n message: %s\n stack: %s', e.name, e.message, e.stack)
+    }
 }
