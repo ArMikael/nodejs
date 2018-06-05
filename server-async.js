1e4f426a1c957144f86917f6fa5bdef2f6ab7371
Michael Treser <armikael2@gmail.com>
2018-06-05 17:30:57 +0300

    Added async server example to prevent blocking other request from server.

---
+++ server-async.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    if (req.url == '/') {
+        fs.readFile('index.html', function(err, info) {
+            if (err) {
+                console.error(err);
+                res.statusCode = 500;
+                res.end("Server Error");
+                return;
+            }
+
+            res.end(info);
+
+        });
+    } else {
+        // 404
+    }
+}).listen(3000);
