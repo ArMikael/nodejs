968371d96e09fa1c77d98b6d14471d24d813aff1
Michael Treser <armikael2@gmail.com>
2018-07-05 21:38:25 +0300

    Changed handlebars to express-handlebars. Configure and prepared to use templating with express-handlebars.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const handlebars = require('handlebars');
+const hbs = require('express-handlebars');
 
-app.set('view engine', 'handlebars');
+app.engine('hbs', hbs({ extname: 'hbs', defaultValue: 'layout', layoutsDir: __dirname + '/views/' }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+   res.send('Welcome Home!');
+});
+
+app.get('/user/:name', (req, res) => {
+    res.render(user);
+});
 
 
+app.listen(3000);
 
-
-let server = require('./server');
-
-server.run(); // Вызов метод дочернего модуля.
+// let server = require('./server');
+//
+// server.run(); // Вызов метод дочернего модуля.
