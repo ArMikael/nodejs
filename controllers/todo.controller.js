c4f01299df0886d02d52389b1f5173d998b3e674
Michael Treser <armikael2@gmail.com>
2018-07-09 23:31:03 +0300

    Created dynamic todo list content. Implement GET and POST todo item methods.

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,11 +1,20 @@
+let data = [
+    { item: 'Sending and handling web forms' },
+    { item: 'User Authentication and Authorization'},
+    { item: 'Connection to DB'}
+];
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 module.exports = (app) => {
 
     app.get('/todo', (req, res) => {
-
+        res.render('todo', {data: data});
     });
 
-    app.post('/todo', (req, res) => {
-
+    app.post('/todo', urlencodedParser, (req, res) => {
+        data.push(req.body);
+        res.render('todo', {data: data});
     });
 
     app.delete('/todo', (req, res) => {
