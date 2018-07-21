c8abd04b45e2da56636d9a53473d9f7dd7b9f2d2
Michael Treser <armikael2@gmail.com>
2018-07-21 23:23:22 +0300

    Implemented basic validation with Joi package.

--- sandbox/courses.js
+++ sandbox/courses.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const Joi = require('joi');
 
 const app = express();
+app.use(express.json());
 
 const courses = [
     {
@@ -23,6 +25,28 @@ app.get('/courses/:id', (req, res) => {
     if (!course) res.status(404).send('The course with the given ID was not found.');
 
     res.status(200).send(course);
+});
+
+app.post('/courses/', (req, res) => {
+    const schema = {
+      name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+        // Bad request 400
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
 });
 
 const port = process.env.PORT || 3200;
