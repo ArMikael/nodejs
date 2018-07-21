1720542f32528b20367cd758342ce88a24b22525
Michael Treser <armikael2@gmail.com>
2018-07-21 17:03:02 +0300

    Proper way to assign PORT to node app.

--- app.js
+++ app.js
@@ -57,8 +57,8 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to the port: ${ port }...`));
 
 // let server = require('./server');
 //
