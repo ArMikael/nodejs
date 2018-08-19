d083595f7c60bd93ab46a060b6279565d71cfb7b
Michael Treser <armikael2@gmail.com>
2018-08-19 13:50:32 +0300

    Hashing passwords with bcrypt.

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 const { User } = require('../models/user.model');
+
 
 router.post('/', async (req, res) => {
     try {
@@ -14,6 +16,11 @@ router.post('/', async (req, res) => {
         if (user) return res.status(400).send('User already registered.');
 
         user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+        // Hashing passwords
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+
         await user.save() ;
 
         res.send(_.pick(user, ['_id', 'name', 'email']));
