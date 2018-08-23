0987d683641830a2273ed1e3ddadb756dbd3815e
Michael Treser <armikael2@gmail.com>
2018-08-23 18:34:35 +0300

    Implemented user token with JWT and private config.

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const Joi = require('joi');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { privateConfig } = require('../config/private-config');
 
 const { User } = require('../models/user.model');
 
@@ -18,7 +20,8 @@ router.post('/', async (req, res) => {
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-        res.send(true);
+        const token = jwt.sign({ _id: user._id }, privateConfig.secretKey);
+        res.send(token);
     }
     catch (ex) {
         console.log(ex.message);
